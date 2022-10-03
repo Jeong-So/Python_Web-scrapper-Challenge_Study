e68e1b8b2a3973aeac2e36af9c546d5ff51b70ea
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2022-10-03 07:07:21 +0000

    job scrapper 실습1

---
+++ Day8/web_scrapper01.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+def extract_jobs(term):
+  url = f"https://remoteok.com/remote-{term}-jobs"
+  request = requests.get(url, headers={"User-Agent": "Kimchi"})
+  if request.status_code == 200:
+    soup = BeautifulSoup(request.text, "html.parser")
+    # write your ✨magical✨ code here
+  else:
+    print("Can't get jobs.")
+
+extract_jobs("rust")
